06dbc0d5b2253b6adb7c7d3a6cce914a218411e9
Saad Amin <saadamin662@gmail.com>
2023-09-20 19:24:34 +0500

    update

--- index.js
+++ index.js
@@ -98,21 +98,4 @@ app.get("/search/:key",  async (req, resp) => {
     resp.send(result);
 });
 
-// function verifyToken(req, resp, next) {
-//     let token = req.headers['authorization'];
-//     if (token) {
-//         token = token.split(' ')[1];
-//         console.warn("middleware called if", token);
-//         Jwt.verify(token, jwtKey, (err, valid) => {
-//             if (err) {
-//                 resp.status(401).send({ result: "Please provide valid token" });
-//             } else {
-//                 next();
-//             }
-//         });
-//     } else {
-//         resp.status(403).send({ result: "Please add token with header" });
-//     }
-// }
-
 app.listen(5000);
